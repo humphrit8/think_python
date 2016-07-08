1f8f3f06291b6bf4c206f4cfb73c8124b00e17ea
humphrit8 <thom2@bgs.ac.uk>
2016-07-08 16:15:33 +0100

    Adding stuff from office 5

--- word_frequency_analysis.py
+++ word_frequency_analysis.py
@@ -4,6 +4,7 @@ def readfile(filename):
     file = open(filename)
     l = file.readlines()
     l = strip_gut_header(l)
+    l = strip_gut_footer(l)
     #print (l[0:5])
     spc = string.whitespace + string.punctuation
     words = []
@@ -13,6 +14,7 @@ def readfile(filename):
         thing = item.split(" ")
         for word in thing:
             word = word.strip(spc)
+            word = word.translate(spc)
             if len(word) > 0:
                 words.append(word.lower())
     print ('Total No. words in book:', len(words))
@@ -31,6 +33,13 @@ def strip_gut_header(plan):
     print (plan[start_line])
     return plan[start_line:]
 
+def strip_gut_footer(plan):
+    for i in range(len(plan)-500, len(plan)):
+        if plan[i][0:28] == 'End of the Project Gutenberg':
+            footer = i
+            break
+    return plan[:footer]
+
 def each_word_used(book):
     word_times = dict()
     for word in book:
@@ -40,9 +49,36 @@ def each_word_used(book):
             word_times[word] = 1
     return word_times
 
+def twenty_most_common(word_dict):
+    values = []
+    for key in word_dict:
+        values.append(word_dict[key])
+    values.sort(reverse = True)
+    print (values[0], values[10])
+    for key in word_dict:
+        if word_dict[key] == 1780:
+            print (word_dict[key])
+    for i in range(0, 30):
+        for key in word_dict:
+            if word_dict[key] == values[i]:
+                print (values[i], key)
+                #pass
 
-t = each_word_used(readfile('C:\\Users\\Thomas\\Documents\\python\\pg5670.txt'))
+def check_word_list(word_dict):
+    file = open('words.txt')
+    wordlist = file.readlines()
+    for word in wordlist:
+        word.strip()
+    strangers = []
+    for key in word_dict:
+        if key not in wordlist:
+            strangers.append(key)
+    return strangers
+
+
+t = each_word_used(readfile('jacobs.txt'))
 print ('No. different words:', len(t))
 
-
+twenty_most_common(t)
+print (check_word_list(t))
 
